f71652325e6fd5374f48bd11bb2f3a47310ee6ee
Per Holmäng <per.holmang@gmail.com>
2023-12-06 10:24:12 +0100

    day06: today was easy

---
+++ day06/index.ts
@@ -0,0 +1,63 @@
+import fs from 'fs';
+
+type Race = { time: number; distance: number };
+
+function read(file: string, singleRace = false): Race[] {
+    const contents = fs.readFileSync(file, 'utf8').split('\n');
+    const timesStr = contents[0].split(':')[1].trim();
+    const distancesStr = contents[1].split(':')[1].trim();
+
+    if (singleRace) {
+        const time = parseInt(timesStr.replace(/\s+/g, '').trim(), 10);
+        const distance = parseInt(distancesStr.replace(/\s+/g, '').trim(), 10);
+        return [{ time, distance }];
+    }
+
+    function splitAndParse(input: string): number[] {
+        return input
+            .replace(/\s+/g, ' ')
+            .split(' ')
+            .map((t) => parseInt(t.trim(), 10));
+    }
+
+    const times = splitAndParse(timesStr);
+    const distances = splitAndParse(distancesStr);
+
+    const races = times.reduce<Race[]>((acc, curr, i) => {
+        return [...acc, { time: curr, distance: distances[i] }];
+    }, []);
+
+    return races;
+}
+
+function calculateDistance(race: Race, gasTime: number): number {
+    return (race.time - gasTime) * gasTime;
+}
+function waysToBeatRace(race: Race): number {
+    let waysToBeat = 0;
+    for (let j = 1; j < race.time; j++) {
+        if (calculateDistance(race, j) > race.distance) {
+            waysToBeat++;
+        }
+    }
+    return waysToBeat;
+}
+
+function part1() {
+    const races = read('input.txt');
+    return races
+        .map((race) => waysToBeatRace(race))
+        .reduce((acc, curr) => acc * curr, 1);
+}
+
+function part2() {
+    const race = read('input.txt', true)[0];
+
+    return waysToBeatRace(race);
+}
+
+if (process.env.part === 'part1') {
+    console.log(part1());
+} else {
+    console.log(part2());
+}
