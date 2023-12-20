70bb41a6da33b3dd8c607d7ef3ffbb15e4d3ece8
Per Holmäng <per.holmang@gmail.com>
2023-12-20 16:45:12 +0100

    day18: code cleanup

--- day18/index.ts
+++ day18/index.ts
@@ -15,6 +15,7 @@ const lines = parser.lines;
 
 function build_path(lines: Instruction[], decode_hex: boolean = false) {
     let pos: [number, number] = [0, 0];
+    let total_distance = 0;
     const path: [number, number][] = [[0, 0]];
     for (let i = 0; i < lines.length; i++) {
         let direction = lines[i][0];
@@ -24,6 +25,8 @@ function build_path(lines: Instruction[], decode_hex: boolean = false) {
             direction = parsed[0];
             distance = parsed[1];
         }
+
+        total_distance += distance;
 
         switch (direction) {
             case 'U':
@@ -44,11 +47,7 @@ function build_path(lines: Instruction[], decode_hex: boolean = false) {
 
         path.push([pos[0], pos[1]]);
     }
-    return path;
-}
-
-function hex2dec(hex: string) {
-    return parseInt(hex, 16);
+    return { path, total_distance };
 }
 
 function parse_hex(hex: string): [Direction, number] {
@@ -68,25 +67,11 @@ function shoelace(paths: [number, number][]) {
     return sum / 2;
 }
 
-function perimeter(path: [number, number][]) {
-    let perimeter = 0;
-    for (let i = 0; i < path.length - 1; i++) {
-        const f = path[i];
-        const t = path[i + 1];
-        if (f[0] === t[0]) {
-            perimeter += Math.abs(f[1] - t[1]);
-        } else {
-            perimeter += Math.abs(f[0] - t[0]);
-        }
-    }
-    return perimeter;
-}
-
 if (process.env.part === 'part1') {
     // picks theorem!
-    const path = build_path(lines);
-    console.log(shoelace(path) + perimeter(path) / 2 + 1);
+    const { path, total_distance } = build_path(lines);
+    console.log(shoelace(path) + total_distance / 2 + 1);
 } else if (process.env.part === 'part2') {
-    const path = build_path(lines, true);
-    console.log(shoelace(path) + perimeter(path) / 2 + 1);
+    const { path, total_distance } = build_path(lines, true);
+    console.log(shoelace(path) + total_distance / 2 + 1);
 }
